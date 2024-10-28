def67cd010f9a3ae63ec591ede7d1656a8953aa4
Rizki Shafara Adiyatma <79394478+rizkishafara@users.noreply.github.com>
2024-10-28 16:24:56 +0700

    fix:color

--- src/components/Fragments/Header.jsx
+++ src/components/Fragments/Header.jsx
@@ -2,15 +2,16 @@ import Input from "../Elements/LabeledInput/Input";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 
 const Header = () => {
+  const date = new Date().toDateString().slice(4);
   return (
     <header className="border-b-2 p-6 flex items-center justify-between">
       <div className="flex">
         <div className="font-bold text-lg">Username</div>
-        <div className="ms-6 text-gray-400  ">Oct 17, 2024</div>
+        <div className="ms-6 text-gray-03  ">{date}</div>
       </div>
       <div className="flex">
         <div>
-          <NotificationsIcon className="text-gray-500 scale-110" />
+          <NotificationsIcon className="text-gray-01 scale-110" />
         </div>
         <div className="ms-10 hidden sm:block">
           <Input type="text" placeholder="Search" variant="bg-white w-80" />
--- src/components/Fragments/Navbar.jsx
+++ src/components/Fragments/Navbar.jsx
@@ -48,14 +48,14 @@ const Navbar = () => {
     },
   ];
   return (
-    <nav className="bg-gray-900 text-gray-50 sm:w-72 w-36 min-h-screen px-7 py-12 flex flex-col justify-between">
+    <nav className="bg-defaultBlack text-special-bg2 sm:w-72 w-36 min-h-screen px-7 py-12 flex flex-col justify-between">
       <div>
         <div className="flex justify-center mb-10">
           <Logo variant="text-white text-2xl" />
         </div>
         {menus.map((menu) => (
           <Link key={menu.id} to={menu.link}>
-            <div className="flex hover:bg-gray-700 hover:text-white px-4 py-3 rounded-md">
+            <div className="flex hover:bg-special-bg3 hover:text-white px-4 py-3 rounded-md">
               <div className="mx-auto sm:mx-0">{menu.icon}</div>
               <div className="ms-3 hidden sm:block">{menu.label}</div>
             </div>
@@ -63,11 +63,11 @@ const Navbar = () => {
         ))}
       </div>
       <div className="sticky bottom-12">
-        <div className="flex bg-gray-700 px-4 py-3 rounded-md">
+        <div className="flex bg-special-bg3 px-4 py-3 rounded-md">
           <div className="mx-auto sm:mx-0">
             <Icon.Logout />
           </div>
-          <div className="ms-3 text-gray-50 hidden sm:block">Logout</div>
+          <div className="ms-3 text-special-bg3 hidden sm:block">Logout</div>
         </div>
         <div className="border-b my-10 border-b-special-bg"></div>
         <div className="flex justify-between">
