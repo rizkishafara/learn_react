12deeecefdff0156efcf12e505fe82e723f35f03
Rizki Shafara Adiyatma <79394478+rizkishafara@users.noreply.github.com>
2024-11-01 10:05:57 +0700

    icon google

--- src/components/Layouts/AuthLayout.jsx
+++ src/components/Layouts/AuthLayout.jsx
@@ -13,7 +13,12 @@ const Authlayout = (props) => {
           <div className="py-7 flex justify-center text-xs text-gray-03">
             <span className="bg-white px-5"> or {title} with</span>
           </div>
-          <Button variant="bg-gray-05 w-full text-gray-01">
+          <Button variant="bg-gray-05 w-full text-gray-01 flex items-center justify-center space-x-2">
+            <img
+              src="images/google.png"
+              className="w-5 h-5"
+              alt="Google logo"
+            />
             <span className="text-dec">Continue with Google</span>
           </Button>
         </>
