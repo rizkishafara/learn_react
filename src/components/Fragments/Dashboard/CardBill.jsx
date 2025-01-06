cf0b140cc9a286e9caf4757c5d3b0c941b11e1c1
Rizki Shafara Adiyatma <79394478+rizkishafara@users.noreply.github.com>
2025-01-07 07:11:22 +0800

    feat: add CardBill component to display upcoming bills

---
+++ src/components/Fragments/Dashboard/CardBill.jsx
@@ -0,0 +1,83 @@
+import { useEffect, useState } from "react";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import Card from "../../Elements/Card";
+import axios from "axios";
+import { NotifContext } from "../../../context/notifContext";
+import { AuthContext } from "../../../context/authContext";
+
+const CardBill = () => {
+  const { msg, setMsg, setOpen, setIsLoading } = useContext(NotifContext);
+  const { setIsLoggedIn, setName } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const [bill, setBill] = useState({ bills: [] });
+
+  const getData = async () => {
+    try {
+      const refreshToken = localStorage.getItem("refreshToken");
+
+      const response = await axios.get(
+        "https://jwt-auth-eight-neon.vercel.app/bills",
+        {
+          headers: {
+            Authorization: `Bearer ${refreshToken}`,
+          },
+        }
+      );
+      setBill({ bills: response.data.data });
+    } catch (error) {
+      if (error.response) {
+        if (error.response.status == 401) {
+          setOpen(true);
+          setMsg({
+            severity: "error",
+            desc: "Session Has Expired. Please Login.",
+          });
+
+          setIsLoggedIn(false);
+          setName("");
+
+          localStorage.removeItem("refreshToken");
+          navigate("/login");
+        } else {
+          console.log(error.response);
+        }
+      }
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <Card
+      title="Upcoming Bill"
+      desc={bill.bills.map((bill) => {
+        return (
+          <div key={bill.id} className="lg:flex justify-between pt-3 pb-3">
+            <div className="flex">
+              <div className="bg-special-bg me-3 px-4 rounded-lg flex place-content-center flex-col">
+                <span className="text-xs">{bill.month}</span>
+                <span className="text-2xl font-bold">{bill.date}</span>
+              </div>
+              <div className="">
+                <img className="h-6" src={`/images/${bill.logo}`} />
+                <span className="font-bold">{bill.name}</span>
+                <br />
+                <span className="text-xs">Last Charge - {bill.lastCharge}</span>
+              </div>
+            </div>
+            <div className="flex place-content-center flex-col">
+              <span className="p-2 border rounded-lg font-bold text-center">
+                ${bill.amount}
+              </span>
+            </div>
+          </div>
+        );
+      })}
+    ></Card>
+  );
+};
+
+export default CardBill;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import CardBalance from "../components/Fragments/Dashboard/CardBalance";
 import CardStatistic from "../components/Fragments/Dashboard/CardStatistic";
 import CardGoal from "../components/Fragments/Dashboard/CardGoal";
+import CardBill from "../components/Fragments/Dashboard/CardBill";
 
 const DashboardPage = () => {
   const tabs = ["All", "Revenue", "Expense"];
@@ -24,27 +25,27 @@ const DashboardPage = () => {
     setTrxs(transactionFiltered);
   }
 
-  const billCard = bills.map((bill) => (
-    <div key={bill.id} className="lg:flex justify-between pt-3 pb-3">
-      <div className="flex">
-        <div className="bg-special-bg me-3 px-4 rounded-lg flex place-content-center flex-col">
-          <span className="text-xs">{bill.month}</span>
-          <span className="text-2xl font-bold">{bill.date}</span>
-        </div>
-        <div className="">
-          <img className="h-6" src={`/images/${bill.logo}`} />
-          <span className="font-bold">{bill.name}</span>
-          <br />
-          <span className="text-xs">Last Charge - {bill.lastCharge}</span>
-        </div>
-      </div>
-      <div className="flex place-content-center flex-col">
-        <span className="p-2 border rounded-lg font-bold text-center">
-          ${bill.amount}
-        </span>
-      </div>
-    </div>
-  ));
+  // const billCard = bills.map((bill) => (
+  //   <div key={bill.id} className="lg:flex justify-between pt-3 pb-3">
+  //     <div className="flex">
+  //       <div className="bg-special-bg me-3 px-4 rounded-lg flex place-content-center flex-col">
+  //         <span className="text-xs">{bill.month}</span>
+  //         <span className="text-2xl font-bold">{bill.date}</span>
+  //       </div>
+  //       <div className="">
+  //         <img className="h-6" src={`/images/${bill.logo}`} />
+  //         <span className="font-bold">{bill.name}</span>
+  //         <br />
+  //         <span className="text-xs">Last Charge - {bill.lastCharge}</span>
+  //       </div>
+  //     </div>
+  //     <div className="flex place-content-center flex-col">
+  //       <span className="p-2 border rounded-lg font-bold text-center">
+  //         ${bill.amount}
+  //       </span>
+  //     </div>
+  //   </div>
+  // ));
   const transactionCard = trxs.map((transaction) => (
     <div key={transaction.id} className="flex justify-between my-6">
       <div className="flex">
@@ -99,7 +100,7 @@ const DashboardPage = () => {
         {/* <Card title="Total Balance" /> */}
         <CardGoal />
         {/* <Card title="Goals" /> */}
-        <Card title="Upcoming Bill" desc={billCard} />
+        <CardBill />
         <Card
           variant="md:col-span-1 md:row-span-2"
           title="Recent Transaction"
