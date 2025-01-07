67f9c883a5a43793814a30a0f5628a3b6a5d6b5d
Rizki Shafara Adiyatma <79394478+rizkishafara@users.noreply.github.com>
2025-01-07 22:54:37 +0800

    feat: update user login test to verify access to dashboard and user name persistence

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,23 +1,27 @@
-describe("User login", () => {
+describe("User Access to Dashboard", () => {
   it("should not allow user to log in with invalid credentials", () => {
-    cy.visit("http://localhost:5173/");
+    cy.visit("http://localhost:5173");
 
     cy.url().should("include", "/login");
 
     cy.get("input#email")
       .should("be.visible")
       .should("have.attr", "placeholder", "hello@example.com")
-      .type("helloexample.com")
-      .should("have.value", "helloexample.com");
+      .type("111202315431@mhs.dinus.ac.id")
+      .should("have.value", "111202315431@mhs.dinus.ac.id");
 
     cy.get("input#password")
       .should("be.visible")
       .should("have.attr", "placeholder", "*************")
       .type("123456")
-      .should("have.value", "123");
+      .should("have.value", "123456");
 
     cy.get("button").contains("Login").click();
 
-    cy.get("div").contains("Invalid email format");
-  })
-})
+    cy.get("div").contains("Rizki").should("be.visible");
+
+    //cek apakah nama user tetap muncul setelah reload
+    cy.reload();
+    cy.get("div").contains("Rizki").should("be.visible");
+  });
+});
