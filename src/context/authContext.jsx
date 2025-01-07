44468aeec6dec0ab40a0c7953bb0dd6d3ab262d9
Rizki Shafara Adiyatma <79394478+rizkishafara@users.noreply.github.com>
2025-01-07 20:46:02 +0800

    feat: persist user name in local storage and update context handling

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -1,15 +1,23 @@
 import { createContext, useState } from "react";
 
+const INITIAL_NAME = localStorage.getItem("name") || "";
 const INITIAL_STATE = localStorage.getItem("refreshToken") ? true : false;
 
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(INITIAL_STATE);
-  const [name, setName] = useState("");
+  const [name, setName] = useState(INITIAL_NAME);
+
+  const handleName = (namabaru) => {
+    setName(namabaru);
+    localStorage.setItem("name", namabaru);
+  };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn, name, setName }}>
+    <AuthContext.Provider
+      value={{ isLoggedIn, setIsLoggedIn, name, setName: handleName }}
+    >
       {children}
     </AuthContext.Provider>
   );
